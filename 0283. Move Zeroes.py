a0a2ad889e9cb6bf3b60c4ba70f8d9e4bac1c585
Hosein Hamedi <hoseinit@users.noreply.github.com>
2025-01-29 19:12:52 +0100

    Update 0283. Move Zeroes.py

--- 0283. Move Zeroes.py
+++ 0283. Move Zeroes.py
@@ -1,9 +1,8 @@
-#Inefficient O(n²) solution
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                for j in range(i, len(nums)-1):
-                    nums[j], nums[j+1] = nums[j+1], nums[j]
-
+        write_ptr = 0
+        for read_ptr in range(len(nums)):
+            if nums[read_ptr] != 0:
+                nums[read_ptr], nums[write_ptr] = nums[write_ptr], nums[read_ptr]
+                write_ptr += 1
 
