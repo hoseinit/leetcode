2059edfadad84863dd12a576bfc3fc4b89ac4fc3
Hosein Hamedi <hoseinit@users.noreply.github.com>
2025-03-16 16:12:30 +0100

    Update 0104. Maximum Depth of Binary Tree.py

--- 0104. Maximum Depth of Binary Tree.py
+++ 0104. Maximum Depth of Binary Tree.py
@@ -24,3 +24,18 @@ class Solution:
                     q.append(node.right)
             level += 1
         return level
+
+# Using iterative DGS
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+            
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
