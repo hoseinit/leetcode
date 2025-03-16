0c85af71f0c43847e90d233b3040051444908330
Hosein Hamedi <hoseinit@users.noreply.github.com>
2025-03-16 16:04:15 +0100

    Update 0104. Maximum Depth of Binary Tree.py

--- 0104. Maximum Depth of Binary Tree.py
+++ 0104. Maximum Depth of Binary Tree.py
@@ -4,3 +4,23 @@ class Solution:
         if not root:
             return 0
         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+
+# Using BFS
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        level = 0
+        q = deque([root])
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
