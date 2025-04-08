bd00f8d002e6f49d805e35cf8bea9a95137717fe
Hosein Hamedi <hoseinit@users.noreply.github.com>
2025-04-08 19:21:54 +0200

    Update 1071. Greatest Common Divisor of Strings.py

--- 1071. Greatest Common Divisor of Strings.py
+++ 1071. Greatest Common Divisor of Strings.py
@@ -10,3 +10,10 @@ class Solution:
 
         length = gcd(len(str1), len(str2))
         return str1[:length]
+
+
+
+# one liner
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        return str1[:math.gcd(len(str1), len(str2))] if str1 + str2 == str2 + str1 else ""
